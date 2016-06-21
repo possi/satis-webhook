46b830dab66190b5adf9aa452b55595e6d317450
Jascha Starke <j.starke@meeva.de>
2016-06-21 15:35:57 +0200

    Misc Improvements for gitlab webhook

--- webhook.php
+++ webhook.php
@@ -4,6 +4,8 @@ require_once __DIR__.'/vendor/autoload.php';
 use Symfony\Component\Process\Process;
 use Symfony\Component\Yaml\Yaml;
 
+header('Content-Type: text/html; charset=utf-8');
+
 if (!file_exists(__DIR__.'/config.yml')) {
     echo "Please, define your satis configuration in a config.yml file.\nYou can use the config.yml.dist as a template.";
     exit(-1);
@@ -11,12 +13,21 @@ if (!file_exists(__DIR__.'/config.yml')) {
 
 $defaults = array(
     'bin' => 'bin/satis',
+    'options' => '',
     'json' => 'satis.json',
     'webroot' => 'web/',
     'user' => null,
+    'token' => null,
 );
 $config = Yaml::parse(__DIR__.'/config.yml');
 $config = array_merge($defaults, $config);
+
+if ($config['token']) {
+    if (!isset($_SERVER['HTTP_X_GITLAB_TOKEN']) || $_SERVER['HTTP_X_GITLAB_TOKEN'] != $config['token']) {
+        header("{$_SERVER['SERVER_PROTOCOL']} 403 Forbidden", true, 403);
+        exit(1);
+    }
+}
 
 $errors = array();
 if (!file_exists($config['bin'])) {
@@ -32,6 +43,7 @@ if (!file_exists($config['webroot'])) {
 }
 
 if (!empty($errors)) {
+    header("{$_SERVER['SERVER_PROTOCOL']} 500 Internal Server Error", true, 500);
     echo 'The build cannot be run due to some errors. Please, review them and check your config.yml:'."\n";
     foreach ($errors as $error) {
         echo '- '.$error."\n";
@@ -39,19 +51,46 @@ if (!empty($errors)) {
     exit(-1);
 }
 
-$command = sprintf('%s build %s %s', $config['bin'], $config['json'], $config['webroot']);
+$command = sprintf('%s %s build %s %s', $config['bin'], $config['options'], $config['json'], $config['webroot']);
 if (null !== $config['user']) {
     $command = sprintf('sudo -u %s -i %s', $config['user'], $command);
 }
 
-$process = new Process($command);
-$exitCode = $process->run(function ($type, $buffer) {
-    if ('err' === $type) {
-        echo 'E';
-        error_log($buffer);
-    } else {
-        echo '.';
-    }
-});
+function execute_build() {
+    error_log("execute_build");
+    global $command, $config;
+    $process = new Process($command);
+    if (isset($config['env']))
+        $process->setEnv($config['env']);
 
-echo "\n\n" . ($exitCode === 0 ? 'Successful rebuild!' : 'Oops! An error occured!') . "\n";
+    $exitCode = $process->run(function ($type, $buffer) {
+        if ('err' === $type) {
+            echo 'E: ', $buffer, PHP_EOL;
+            error_log($buffer);
+        } else {
+            echo '.';
+        }
+    });
+
+    error_log("execute_build done");
+    echo "\n\n" . ($exitCode === 0 ? 'Successful rebuild!' : 'Oops! An error occured!') . "\n";
+}
+
+if (!empty($_SERVER['HTTP_X_GITLAB_EVENT'])) {
+    // Responde as quick as possible
+    ini_set('implicit_flush', true);
+    ignore_user_abort(true);
+    header("Connection: close", true);
+    header("Content-Length: 2", true);
+    ob_end_flush();
+    //register_shutdown_function("execute_build");
+    if (function_exists('fastcgi_finish_request'))
+        fastcgi_finish_request();
+    echo "ok";
+    flush();
+    echo str_pad("", 64*1024);flush();
+    execute_build();
+    exit(0);
+} else {
+    execute_build();
+}
